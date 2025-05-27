8c45f2cc3a80769169a93821c3fe50aeff72ab90
Richard Gonzalez <richardags@hotmail.com>
2025-05-27 00:06:39 -0300

    add CI pipeline for user service

---
+++ services/user-service/test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /health', () => {
+  it('should return healthy status', async () => {
+    const res = await request(app).get('/health');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('status', 'OK');
+  });
+});
