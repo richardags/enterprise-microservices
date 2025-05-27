468470590e4cb74032cc2f418daccbf3faa788d0
Richard Gonzalez <richardags@hotmail.com>
2025-05-27 01:07:28 -0300

    test5 workflow

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
 });
 
-PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 
-module.exports = app;
-//test
+module.exports = { app, server };
+//test2
