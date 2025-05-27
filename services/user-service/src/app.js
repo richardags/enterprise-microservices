84f5c2e1843ce7571b513c2c830a5a0f0cbec31f
Richard Gonzalez <richardags@hotmail.com>
2025-05-26 23:20:03 -0300

    Create services/user-service/src/app.js

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -5,4 +5,10 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
 });
 
+PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 module.exports = app;
